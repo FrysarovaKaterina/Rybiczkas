70aaaf79c18ea78bb13ee2bb923dbd1df79ad97a
Fryšarová, Kateřina <frysakat@fel.cvut.cz>
2023-04-29 09:03:46 +0200

    KeyListener (attached to Stage in HelloAp, puts KeyCodes to a HashSet in GameEngine

--- src/main/java/com/example/game/GameEngine.java
+++ src/main/java/com/example/game/GameEngine.java
@@ -1,12 +1,20 @@
 package com.example.game;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Random;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GameEngine implements Runnable {
     GraphicsContext gc;
+    private Set<KeyCode> keysPressed = new HashSet<>();
     // todo list of pressed keys
 
     public GameEngine(GraphicsContext gc) {
@@ -23,17 +31,33 @@ public class GameEngine implements Runnable {
     public void run () {
         while (true) {
             gc.clearRect(0,0,gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+            // Check if the up arrow key is pressed
+
 
             // energy, lives .setValue todo
             // detect collisions todo
             // call move todo
             // call render todo
 
+
+            System.out.println(keysPressed);
+
             try {
                 Thread.sleep(1000/60);
+                //System.out.println(keysPressed);
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
     }
+
+    public void addKeyToSet (KeyCode key){
+        keysPressed.add (key);
+    }
+    public void removeKeyFromSet (KeyCode keyrem){
+        keysPressed.remove(keyrem);
+    }
+
+
 }
--- src/main/java/com/example/game/HelloApplication.java
+++ src/main/java/com/example/game/HelloApplication.java
@@ -1,14 +1,20 @@
 package com.example.game;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+
+import java.awt.Component;
+import java.awt.event.KeyAdapter;
 
 import java.io.IOException;
 
@@ -19,10 +25,20 @@ public class HelloApplication extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         GameEngine engine = new GameEngine(gc);
-        // todo hook up keylistener to gameengine
+        // todo hook up keylistener to gameengine wtf how
+
+
         new Thread(engine).start();
 
         Scene scene = new Scene(new Group(canvas),1920, 1080);
+        scene.setOnKeyPressed(keyEvent -> {
+            KeyCode code = keyEvent.getCode();
+            engine.addKeyToSet(code);
+        });
+        scene.setOnKeyReleased(keyEvent ->{
+            KeyCode coderem = keyEvent.getCode();
+            engine.removeKeyFromSet(coderem);
+        });
         stage.setTitle("gaming");
         stage.setScene(scene);
         stage.show();
